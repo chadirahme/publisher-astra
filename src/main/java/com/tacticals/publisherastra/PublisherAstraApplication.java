738c121f3d79bcfbbb2bd960723065798a611b49
Chadi Rahme <eng.chadi@gmail.com>
2024-11-17 20:05:50 -0500

    first commit

---
+++ src/main/java/com/tacticals/publisherastra/PublisherAstraApplication.java
@@ -0,0 +1,13 @@
+package com.tacticals.publisherastra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublisherAstraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublisherAstraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tacticals/publisherastra/PublisherAstraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tacticals.publisherastra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublisherAstraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
